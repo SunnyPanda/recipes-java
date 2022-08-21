a9c1983eda5619e95b906db0ed63b2b4e4c35f21
SunnyPanda <kashtanka.nya@gmail.com>
2022-08-21 20:23:19 +0300

    Store multiple recipes, get them by id

--- Recipes/task/src/recipes/RecipesController.java
+++ Recipes/task/src/recipes/RecipesController.java
@@ -1,20 +1,34 @@
 package recipes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/recipe")
 public class RecipesController {
 
-    private Recipe recipe;
+    public static Long id = 0L;
+    private final Map<Long, Recipe> recipes = new HashMap<>();
 
-    @GetMapping
-    public Recipe getRecipe() {
-        return recipe;
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> getRecipe(@PathVariable Long id) {
+        Recipe recipe = recipes.get(id);
+        return recipe == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                              : new ResponseEntity<>(recipe, HttpStatus.OK);
     }
 
-    @PostMapping
-    public void saveRecipe(@RequestBody Recipe newRecipe) {
-        recipe = newRecipe;
+    @PostMapping("/new")
+    public Response saveRecipe(@RequestBody Recipe newRecipe) {
+        Long newId = generateId();
+        recipes.put(newId, newRecipe);
+        return new Response(newId);
+    }
+
+    private Long generateId() {
+        return ++id;
     }
 }
